882213f215070417b2b835f635a533cbf0e93241
Jawad Ateeq <jawad.atiq@gmail.com>
2014-03-02 13:35:36 -0500

    - Modified string parsing for received data 	- looking for 'T' instead of 't' 	- coverting to int instead of float - Added error checking if statements - 'getForce' function now returns boolean isntead of nothing - Servo moves according to IR sensor reading if remote not touching anything (only for testing)

--- Arudino_code/controller/controller.ino
+++ Arudino_code/controller/controller.ino
@@ -49,16 +49,17 @@ int strToInt(String string)
 	return atoi(floatbuf);
 }
 
-void getForce( String string, int * num1 )
+boolean getForce( String string, int * num1 )
 {
-	int index1 = 0;
-	int index2 = 0;
-	int index3 = 0;
+	
+int index1 = -1;
+	int index2 = -1;
+	int index3 = -1;
 	int temp = 0;
 
 	while( temp < string.length() )
 	{
-		if (string[temp] == 't')
+		if (string[temp] == 'T')
 			index1 = temp;
 		else if (string[temp] == '/' )
 			index2 = temp;
@@ -67,8 +68,16 @@ void getForce( String string, int * num1 )
 		temp++;
 	}
 
-	String string1 = string.substring( index1+1, index2-1 );
-	*num1 = strToInt( string1 );
+        if (index1 < index2 && index1 != -1 && index2 != -1)
+        {
+      	 // String string1 = string.substring( index1+1, index2-1 );
+      	  //*num1 = strToInt( string1 );
+          *num1 = (string[index1+1]-'0');
+
+          return true;
+        } else {
+           return false;
+        }
 }
 
 void setup() 
@@ -83,10 +92,9 @@ void setup()
 void loop() 
 {
 	/* read the IR sensor and convert to voltage*/
-	IR_val_1 = analogRead(IR_pin_1) * IR_res;
-	IR_val_2 = analogRead(IR_pin_2) * IR_res;	
-	print1IRval(IR_val_1);
-
+	IR_val_1 = analogRead(IR_pin_1);
+	IR_val_2 = analogRead(IR_pin_2);	
+	print1IRval(IR_val_1*IR_res);
 	/* get the force being applied to the remote robot */
 	serialMsg = "";
 	while ( Serial.available() > 0 )
@@ -94,21 +102,20 @@ void loop()
 		char inChar = Serial.read();
 		serialMsg += inChar;
 	}
-
-	getForce( serialMsg, &force_1 );
-	/* move servo */
-	if (force_1 == 1) //object detected
-	{
-		//hold position
-	}
-	else if (force_1 == 0)
-	{
-		servo_val_1 = map(IR_val_1, 0, 1023, 0, 179); // scale it to use it with the servo (value between 0 and 180)
-		servo_obj_1.write(servo_val_1);                  // sets the servo position according to the scaled value 
-	}
-	
+        force_1 = -1;
+	if (getForce( serialMsg, &force_1 ))
+        {
+          	/* move servo */
+        	if (force_1  == 1) //object detected
+        	{
+        		//hold position
+        	}
+        	else if (force_1 == 0)
+        	{
+        		servo_val_1 = map(IR_val_1, 0, 1023, 0, 179); // scale it to use it with the servo (value between 0 and 180)
+            		servo_obj_1.write(servo_val_1);                  // sets the servo position according to the scaled value 
+         	}
+        }        	
+        
 	delay(15);                           // waits for the servo to get there
 }
-
-
-
